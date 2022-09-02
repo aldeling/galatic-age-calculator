8927cdb993441a878274eea2b7a16017db517738
Athea DeLing <almdeling@gmail.com>
2022-09-02 13:57:30 -0700

    test 11 fail

--- __tests__/planetAge.test.js
+++ __tests__/planetAge.test.js
@@ -52,4 +52,8 @@ describe('PlanetAge', () => {
     expect(jupiterRemainingAge.calculateJupiterRemaining()).toEqual(877.64);
   })
 
+  test('should return a positive number is age is greater then expectance', () => {
+    const mercuryRemainingAge = new PlanetAge(80);
+    expect(mercuryRemainingAge.calculateMercuryRemaining()).toEqual(.24)
+  })
 });
