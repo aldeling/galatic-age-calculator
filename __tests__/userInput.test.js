218bc39f2fa67f059d88548b5ce7ebd8676b61df
Athea DeLing <almdeling@gmail.com>
2022-09-02 09:08:47 -0700

    first test fail

--- __tests__/userInput.test.js
+++ __tests__/userInput.test.js
@@ -1,6 +1,8 @@
+import UserInput from './../src/userInput.js';
 
-describe('userInput', () => {
-  test('', () => {
-
+describe('UserInput', () => {
+  test('should correctly create a userinput object with age', () => {
+    const userInput = new UserInput(5);
+    expect(userInput.age).toEqual(5);
   })
 });
