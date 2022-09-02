827f2f567047fb546afef95e2b5c7b2ae9986223
Athea DeLing <almdeling@gmail.com>
2022-09-02 16:15:05 -0700

    update ui, remove extra semi colons

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import PlanetAge from './planetAge';
 
 function handelFormSubmission(event){
   event.preventDefault();
-
+  const age = document.querySelector('#number').value;
+  const planetAge = new PlanetAge(age);
+  const ageOne = planetAge.calculateMercuryAge;
+  document.getElementById("#ageOne").innerText;
+  document.getElementById("#ageTwo").innerText;
+  document.getElementById("#ageThree").innerText;
+  document.getElementById("#ageFour").innerText;
 }
 
 document.querySelector("form").addEventListener("submit", handelFormSubmission);
--- src/planetAge.js
+++ src/planetAge.js
@@ -6,22 +6,22 @@ export default class PlanetAge{
   calculateMercuryAge() {
     let mercuryAge = (this.age * .24);
     return Math.round(mercuryAge*100)/100;
-  };
+  }
 
   calculateVenusAge() {
     let venusAge = (this.age * .62);
     return Math.round(venusAge*100)/100;
-  };
+  }
 
   calculateMarsAge() {
     let marsAge = (this.age * 1.88);
     return Math.round(marsAge*100)/100;
-  };
+  }
 
   calculateJupiterAge() {
     let jupiterAge = (this.age * 11.86);
     return Math.round(jupiterAge*100)/100;
-  };
+  }
 
   calculateMercuryRemaining() {
     let mercuryRemainingAge = ((79 - this.age) * .24);
@@ -29,7 +29,7 @@ export default class PlanetAge{
       return Math.round((mercuryRemainingAge*(-1))*100)/100;
     }
     return Math.round(mercuryRemainingAge*100)/100;
-  };
+  }
 
   calculateVenusRemaining() {
     let venusRemainingAge = ((79 - this.age) * .62);
@@ -37,7 +37,7 @@ export default class PlanetAge{
       return Math.round((venusRemainingAge*(-1))*100)/100;
     }
     return Math.round(venusRemainingAge*100)/100;
-  };
+  }
 
   calculateMarsRemaining() {
     let marsRemainingAge = ((79 - this.age) * 1.88);
@@ -45,7 +45,7 @@ export default class PlanetAge{
       return Math.round((marsRemainingAge*(-1))*100)/100;
     }
     return Math.round(marsRemainingAge*100)/100;
-  };
+  }
 
   calculateJupiterRemaining() {
     let jupiterRemainingAge = ((79 - this.age) * 11.86);
@@ -53,5 +53,5 @@ export default class PlanetAge{
       return Math.round((jupiterRemainingAge*(-1))*100)/100;
     }
     return Math.round(jupiterRemainingAge*100)/100;
-  };
-};
+  }
+}
