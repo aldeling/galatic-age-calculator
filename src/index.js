a4662e41b6d7a621d21275947830b7fa38bc81c0
Athea DeLing <almdeling@gmail.com>
2022-09-02 15:33:26 -0700

    start ui and form

--- src/index.js
+++ src/index.js
@@ -2,3 +2,9 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
+function handelFormSubmission(event){
+  event.preventDefault();
+
+}
+
+document.querySelector("form").addEventListener("submit", handelFormSubmission);
